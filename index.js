46fbbc47a01a9168ca26d9724694dda248ebb33e
Luke Scammell <Luke-Scammell@live.nmit.ac.nz>
2025-02-24 11:21:29 +1300

    Learning number maths with javascript

--- index.js
+++ index.js
@@ -2,4 +2,23 @@
 // Learning JS SDV503 class
 /** multi - line text
  */
-console.log(2+1)
+
+//ESS
+var a = 2
+
+//ES6
+const pi = 3.14
+let c = 4
+
+//+,-, / and *
+console.log(5-2)
+
+let num1 = 5
+let num2 = 10
+const result = num1 *num2
+console.log("result=",result)
+
+let num3 = 20
+let num4 = 5 
+const result1 = num3/num4
+console.log("result1 = ", result1)
